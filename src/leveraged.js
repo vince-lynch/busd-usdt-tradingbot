70fce6ee4cdef13947e7bcb576f972a2b095c4c0
vlshell <77439566+vlshell@users.noreply.github.com>
2021-05-08 22:31:39 +0100

    initial workings of 10x leverage

--- src/leveraged.js
+++ src/leveraged.js
@@ -3,44 +3,54 @@ const FS_MARGIN_DETAILS = `./data/marginAccountDetails.json`;
 
 // Globals
 const symbol = 'BUSDUSDT';
+const isIsolated = 'TRUE';
 
 
+const limitOpen10xLong = (binance, usdtAmount = 12, limitPrice = 0.9981) => {
+    const leveragedAmount = usdtAmount * 10;
 
-/**
- * Borrows to open a limit long
- * @param {} binance 
- * @param {number} amount USD amount, will be leveraged 10x
- */
-const marginOrderLong = (binance, usdtAmount = 12, limitPrice = 0.0984) => {
-	const leveragedAmount = usdtAmount * 10;
-	const isIsolated = 'TRUE';
-	
-	// Need to error handle, it might be that you already have borrowed the money before transaction?? unsure
-	binance.mgBorrow('USDT', leveragedAmount, (error, borrowRes) => {
-    if ( error ) {
-			return console.warn(error);
-		} else {
-    	// Success! Transaction ID: response.tranId
-			console.info("marginOrder response:", borrowRes);
-		}
-		// Start placing the BUY order with the borrowed base asset plus your primary.
-		binance.mgOrder('BUY', symbol, leveragedAmount, limitPrice, { isIsolated: true }, (( error, response ) => {
-			if (error) 
-				return console.warn(error);
+    // Start placing the BUY order with the borrowed base asset plus your primary.
+    binance.mgOrder('BUY', symbol, leveragedAmount, limitPrice, {
+        isIsolated: true,
+        sideEffectType: 'MARGIN_BUY',
+        timeInForce: 'GTC'
+    }, ((error, response) => {
+        if (error) 
+            return console.warn(error);
 
-			console.info("marginOrder response:", response);
-		}), isIsolated) 
-
-	}, isIsolated, symbol);
-		
-		// {
-    //     marginOrder(side, symbol, quantity, price, {
-    //         ...flags,
-    //         isIsolated
-    //     }, callback);
-    // }
+        console.info("marginOrder response:", response);
+    }), isIsolated)
 }
 
+const limitClose10xLong = (binance, fullAmount = 120, limitPrice = 0.9995) => { // beware, fullAmount I think might have gone up, you need to get this from somewhere.
+  // Start placing the BUY order with the borrowed base asset plus your primary.
+  binance.mgOrder('SELL', symbol, fullAmount, limitPrice, {
+      isIsolated: true,
+      sideEffectType: 'AUTO_REPAY', // for close long, you need to 'AUTO_REPAY'
+      timeInForce: 'GTC'
+  }, ((error, response) => {
+      if (error) 
+          return console.warn(error);
+
+      console.info("marginOrder response:", response);
+  }), isIsolated)
+} 
+
+
+const getOpenOrders = (binance) => { /**
+   * Gets open orders
+   * @param {string} symbol - the symbol to get
+   * @param {function} callback - the callback function
+   * @return {undefined}
+   */
+    binance.mgOpenOrders('BUSDUSDT', (error, resOpenOrders) => {
+        if (error) {
+            return console.warn(error);
+        } else { // Success! Transaction ID: response.tranId
+            console.info("getOpenOrders response:", resOpenOrders);
+        }
+    });
+}
 
 const busdUsdtWithLeverage = (binance) => {
     console.log('Starting...');
@@ -50,6 +60,7 @@ const busdUsdtWithLeverage = (binance) => {
         if (error) 
             return console.warn(error);
         
+
         /**
          * Hi, please fix line 4686 at node-binance-api.js:
          * const endpoint = 'v1/margin' + (isIsolated)?'/isolated':'' + '/account'
@@ -59,8 +70,8 @@ const busdUsdtWithLeverage = (binance) => {
         console.info("marginAccountDetails response:", marginAccountDetails);
         fs.writeFileSync(FS_MARGIN_DETAILS, JSON.stringify(marginAccountDetails));
 
-				// make a test margin order
-				marginOrderLong(binance);
+        // make a test margin order
+        limitOpen10xLong(binance);
     }, isIsolated)
 }
 
